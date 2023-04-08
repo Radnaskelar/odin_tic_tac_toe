ffa7e63e532503f72326038e048d4f0e04546c3f
Alex <aleksa.radisav1994@gmail.com>
2023-04-08 19:50:56 +0200

    functionality added, not finished

--- script.js
+++ script.js
@@ -1,47 +1,143 @@
-const gameBoard = (() => {
-  //   const board = ["X", "O", "X", "O"];
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-plusplus */
 
-  const gameButtons = Array.from(
-    document.querySelector(".game-board").children
-  );
+const Gameboard = () => {
+  const board = new Array(9).fill(""); // board state
 
-  //   const render = () => {
-  //     gameButtons.forEach((button) => {
-  //       let index = gameButtons.indexOf(button);
-  //       button.textContent = board[index];
-  //     });
-  //   };
+  const resetBoard = () => new Array(9).fill("");
 
-  const controllerBtns = Array.from(
-    document.querySelector(".controller").getElementsByTagName("button")
-  );
+  const setCell = (index, mark) => {
+    board[index] = mark;
+  };
 
-  let chosenMark = "";
-  function chooseMark(event) {
-    chosenMark = event.target.textContent;
-    return chosenMark;
-  }
+  const checkForWin = (mark) => {
+    const winConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
 
-  controllerBtns.forEach((controllerBtn) =>
-    controllerBtn.addEventListener("click", chooseMark)
-  );
+    const win = winConditions.find((winCondition) =>
+      winCondition.every((index) => board[index] === mark)
+    );
+    if (win === null) return false;
+    return true;
+  }; // not working
 
-  function addMark(event) {
-    if (event.target.textContent === "") {
-      event.target.textContent = chosenMark;
-    }
-  }
-
-  gameButtons.forEach((button) => {
-    button.addEventListener("click", addMark);
-  });
+  const getBoard = () => [...board];
 
   return {
-    chooseMark,
+    resetBoard,
+    setCell,
+    checkForWin,
+    getBoard,
+  };
+};
+
+const Player = (mark, name) => {
+  const getMark = () => mark;
+  const getName = () => name;
+
+  return {
+    getMark,
+    getName,
+  };
+};
+
+const Cell = () => {
+  let value = 0;
+  const addMark = (mark) => {
+    value = mark;
+  };
+  const getValue = () => value;
+  return {
     addMark,
+    getValue,
+  };
+};
+
+const Game = () => {
+  const board = Gameboard();
+
+  const playerX = Player("X", "Player 1");
+  const playerO = Player("O", "Player 2");
+
+  let currentPlayer = playerX;
+  const switchPlayer = () => {
+    if (currentPlayer === playerX) {
+      currentPlayer = playerO;
+    } else {
+      currentPlayer = playerX;
+    }
+  };
+
+  const playRound = (clickedCell) => {
+    console.log(`It is ${currentPlayer.getName()}'s turn.`);
+    board.setCell(clickedCell, currentPlayer.getMark()); // not working
+    console.log(currentPlayer.getMark());
+    board.checkForWin(currentPlayer.getMark());
+    console.log(board.checkForWin());
+    // check for tie
+    switchPlayer();
+  };
+
+  const getCurrentPlayerName = () => currentPlayer.getName();
+
+  return {
+    switchPlayer,
+    playRound,
+    getCurrentPlayerName,
+    setCell: board.setCell,
+    getBoard: board.getBoard,
+    // display winner
+  };
+};
+
+const showUI = (() => {
+  const game = Game();
+  let boardDiv = document.querySelector(".game-board");
+  let playerTurnDiv = document.querySelector("p");
+  const restartButton = document.querySelector("#restartButton");
+
+  let currentPlayer = game.getCurrentPlayerName();
+  const board = game.getBoard(); // not working
+  board.forEach((cell, index) => {
+    const cellBtn = document.createElement("button");
+    cellBtn.classList.add("cell");
+    cellBtn.dataset.cell = index;
+    boardDiv.appendChild(cellBtn);
+  });
+
+  const clickHandlerBoard = (e) => {
+    const clickedCell = e.target.dataset.cell;
+    if (!clickedCell) return;
+
+    game.playRound(clickedCell);
+  };
+
+  const cells = Array.from(boardDiv.children);
+  cells.forEach((cell) => {
+    cell.addEventListener("click", clickHandlerBoard);
+  });
+
+  const displayPlayer = () => {
+    playerTurnDiv.textContent = `${currentPlayer}'s turn!`;
+  };
+
+  const resetScreen = () => {
+    // clear the board on screen
+    boardDiv.textContent = "";
+  };
+
+  return {
+    displayPlayer,
+    resetScreen,
   };
 })();
 
-// const Player = () => {};
-
-// const game = (() => {})();
+showUI.displayPlayer();
